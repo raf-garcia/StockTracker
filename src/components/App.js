a5834ab34440af7b477a6854cb034c1c2fbfc115
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-10 15:22:58 -0400

    Add redux file dependencies

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import '../css/App.css';
+
+// All endpoints are prefixed with: https://api.iextrading.com/1.0 /stock/aapl/price
+
+function App() {
+  return (
+    <div>
+      <h1>Adaptive Stock Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
