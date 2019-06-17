433f840bd55c43d16e79f18ac6c7cc9e2399210f
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-17 13:27:52 -0400

    Refactor API calls inside custom middleware, add chart date to sample state

---
+++ src/util/apiUtil.js
@@ -0,0 +1,38 @@
+export const API_KEY = `pk_4c9d7e256ac94209b05b3ec930d3dd60`;
+
+// filters
+export const newsFilters = [
+  "datetime",
+  "headline",
+  "url",
+  "source"
+].join(',')
+
+export const quoteFilters = [
+  "symbol",
+  "companyName",
+  "open",
+  "close",
+  "high",
+  "low",
+  "latestPrice",
+  "latestVolume",
+  "avgTotalVolume",
+  "marketCap",
+  "peRatio",
+  "week52High",
+  "week52Low"
+].join(',')
+
+export const statsFilters = [
+  "dividendYield"
+].join(',');
+
+export const companyInfoFilters = [
+  "description",
+  "exchange",
+  "sector",
+  "website",
+  "symbol",
+  "companyName"
+].join(',')
