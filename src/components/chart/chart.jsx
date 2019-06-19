07dc18e7771eb01f01430134d7b2357671645b9a
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-06-19 14:29:32 -0400

    made chart responsive

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 import {chartDayData} from './sampleChartData'
 
 const chart = (props) => {
@@ -10,15 +10,17 @@ const chart = (props) => {
     }
 
     return (
-      <AreaChart width={500} height={400} data={data} margin={{
-          top: 10, right: 30, left: 0, bottom: 0,
-        }} >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name"/>
-        <YAxis orientation="right" domain={['dataMin', 'auto']}/>
-        <Tooltip />
-        <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" fillOpacity={0.3} />
-      </AreaChart>
+      <ResponsiveContainer width="70%" aspect={0.9} minWidth={360} maxHeight={800}>
+        <AreaChart data={data} margin={{
+            top: 10, right: 30, left: 0, bottom: 0,
+          }} >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name"/>
+          <YAxis orientation="right" domain={['dataMin', 'auto']}/>
+          <Tooltip />
+          <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" fillOpacity={0.3} />
+        </AreaChart>
+      </ResponsiveContainer>
     );
 }
 export default chart;
