72fe65c136103a6c44b4006cf70334e8e131cff9
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-19 15:48:06 -0400

    Refactor selector to its own file

--- src/components/chart/chartContainer.jsx
+++ src/components/chart/chartContainer.jsx
@@ -1,9 +1,9 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Chart from './chart';
+import { selectChartDataDay } from '../../util/selectors';
 
-const mapStateToProps = ({quotes}) => {
-    const data = quotes.chartDataDay.map((data) => ({name: data.minute, price: data.average}));
-    return {data};
-}
+const mapStateToProps = ({ quotes }) => ({
+    data: selectChartDataDay(quotes.chartDataDay)
+})
 
 export default connect(mapStateToProps)(Chart);
