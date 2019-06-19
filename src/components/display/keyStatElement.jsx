952a8670546fd9b5481a9bef919ac13a57b3bf70
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-19 16:55:43 -0400

    Added spaces

--- src/components/display/keyStatElement.jsx
+++ src/components/display/keyStatElement.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {addCommasToNumber, changeToPercent} from '../../utilities/numberFormatter';
+import {addCommasToNumber } from '../../util/numberFormatter';
 
 const keyStatElement = ({name, value}) => {
-    if(!isNaN(value)){
+    if (!isNaN(value)){
         value = addCommasToNumber(value);
     }
 
-    if(value != null && typeof value == "object")
+    if (value != null && typeof value == "object")
         return <li><span>{name}</span> <span>{value.low}</span> - <span>{value.high}</span></li>
     
     return <li><span>{name}</span> <span>{value}</span></li>
