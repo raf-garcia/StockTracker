6367221f5058b14007947c30b9b9db31ed06330b
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-06-21 12:14:22 -0400

    seperate companyStats selector in selector file

--- src/components/display/keyStatsContainer.jsx
+++ src/components/display/keyStatsContainer.jsx
@@ -1,59 +1,11 @@
 import {connect} from 'react-redux';
 import KeyStatsList from './keyStatsList';
+import {selectCompanyStats} from '../../util/selectors';
 
-const mapStateToProps = (state) => {
-    const {companyStats} = state.quotes;
+const mapStateToProps = ({quotes}) => {
+    const {companyStats} = quotes;
 
-    return {
-        companyStats: [
-            {
-                value: companyStats.close,
-                name: "Previous Close"
-            },    
-            {
-                name: "Day Range",
-                value: {
-                    high: companyStats.high,
-                    low: companyStats.low
-                }
-            },
-            {
-                value: companyStats.latestVolume,
-                name: "Volume"
-            },
-            {
-                value: companyStats.marketCap,
-                name: "Market Cap"
-            },
-            {
-                value: companyStats.peRatio,
-                name: "P/E Ratio"
-            },
-            {
-                value: companyStats.open,
-                name: "Open"
-            },    
-            {
-                name: "52 Week Range",
-                value: {
-                    high: companyStats.week52High,
-                    low: companyStats.week52Low
-                }
-            },
-            {
-                value: companyStats.avgTotalVolume,
-                name: "Total Avg Volume"
-            },
-            {
-                value: companyStats.actualEPS,
-                name: "Earnings Per Share"
-            },
-            {
-                value: companyStats.dividendYield,
-                name: "Dividend & Yield"
-            }
-        ]
-    };
+    return selectCompanyStats(companyStats);
 }
 
-export default connect(mapStateToProps, null)(KeyStatsList);
+export default connect(mapStateToProps)(KeyStatsList);
--- src/util/selectors.js
+++ src/util/selectors.js
@@ -1,3 +1,56 @@
 export const selectChartDataDay = (chartDataDay) => {
   return chartDataDay.filter(data => data.average).map(data => ({ name: data.minute, price: data.average }));
 };
+
+export const selectCompanyStats = (companyData) => {
+  return {
+    companyStats: [
+        {
+            value: companyData.close,
+            name: "Previous Close"
+        },    
+        {
+            name: "Day Range",
+            value: {
+                high: companyData.high,
+                low: companyData.low
+            }
+        },
+        {
+            value: companyData.latestVolume,
+            name: "Volume"
+        },
+        {
+            value: companyData.marketCap,
+            name: "Market Cap"
+        },
+        {
+            value: companyData.peRatio,
+            name: "P/E Ratio"
+        },
+        {
+            value: companyData.open,
+            name: "Open"
+        },    
+        {
+            name: "52 Week Range",
+            value: {
+                high: companyData.week52High,
+                low: companyData.week52Low
+            }
+        },
+        {
+            value: companyData.avgTotalVolume,
+            name: "Total Avg Volume"
+        },
+        {
+            value: companyData.actualEPS,
+            name: "Earnings Per Share"
+        },
+        {
+            value: companyData.dividendYield,
+            name: "Dividend & Yield"
+        }
+    ]
+};
+}
