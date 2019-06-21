a1b85309257c085511424837e3a928532290c9f3
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-21 09:41:18 -0400

    Fix news bug

--- src/reducers/quotes/companyNewsReducer.js
+++ src/reducers/quotes/companyNewsReducer.js
@@ -3,7 +3,7 @@ import { SET_COMPANY_NEWS } from '../../constants/actionTypes';
 const companyNewsReducer = (state = [], action) => {
   switch (action.type) {
     case SET_COMPANY_NEWS:
-      return [...state, action.companyNews];
+      return action.companyNews;
 
     default:
       return state;
