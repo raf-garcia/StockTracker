65ab6e5f4a320b52e2ffda794c13531e457d4f16
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-06-21 12:13:31 -0400

    fix null values for EPS and dividendYield

--- src/reducers/quotes/companyStatsReducer.js
+++ src/reducers/quotes/companyStatsReducer.js
@@ -1,4 +1,8 @@
-import { SET_COMPANY_STATS } from '../../constants/actionTypes';
+import {
+  SET_COMPANY_STATS,
+  SET_COMPANY_EPS,
+  SET_DIVIDENDYIELD
+} from '../../constants/actionTypes';
 
 const defaultState = {
   "actualEPS": null,
@@ -10,6 +14,12 @@ const companyStatsReducer = (state = defaultState, action) => {
     case SET_COMPANY_STATS:
       return Object.assign({}, state, action.companyStats);
 
+    case SET_COMPANY_EPS:
+      return Object.assign({}, state, { "actualEPS": action.earningsPerShare});
+      
+    case SET_DIVIDENDYIELD:
+      return Object.assign({}, state, {"dividendYield": action.dividendYield});
+      
     default:
       return state;
   }
