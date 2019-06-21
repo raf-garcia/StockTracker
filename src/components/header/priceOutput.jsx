2b239e5be50b7d8f91e3c2ff5ab136d0eeb611f8
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-06-21 17:09:11 -0400

    add classNames and styling for header

--- src/components/header/priceOutput.jsx
+++ src/components/header/priceOutput.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const PriceOutput = ({latestPrice, change, changePercent}) => {
     return (
     <>
-        <span>latestPrice: {latestPrice} </span>
-        <span>change: {change} </span>
-        <span>changePercent: {changePercent}</span>
+        <div className="header__price">
+            <span>$ 160.03{latestPrice}</span>
+            <div className="header__price--change">
+                <span>^ 2.94{change}</span>
+                <span>1.88%{changePercent}</span>
+            </div>
+        </div>
     </>
     );
 }
