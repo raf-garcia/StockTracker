b4d76b2056f975e5449b9c62ecfbb86e2c94de41
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-24 12:26:19 -0400

    Refactor reducer to replace top peers array with new values

--- src/reducers/quotes/topPeersReducer.js
+++ src/reducers/quotes/topPeersReducer.js
@@ -3,7 +3,7 @@ import { SET_TOP_PEERS } from '../../constants/actionTypes';
 const topPeersReducer = (state = [], action) => {
   switch (action.type) {
     case SET_TOP_PEERS:
-      return [...state, action.topPeers];
+      return action.topPeers;
 
     default:
       return state;
