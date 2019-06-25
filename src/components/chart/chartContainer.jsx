2e64aa3f6ff09167e69f2ac01e2a431590c60d4b
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-25 12:17:44 -0400

    Add chart data to chart component props

--- src/components/chart/chartContainer.jsx
+++ src/components/chart/chartContainer.jsx
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
 import Chart from './chart';
-import { selectChartDataDay, selectChartDataFiveDay } from '../../util/selectors';
+import { selectChartDataDay, selectChartDataFiveDay, selectChartDataOneMonth, selectChartDataYear } from '../../util/selectors';
 
 const mapStateToProps = ({ quotes }) => ({
     oneDayData: selectChartDataDay(quotes.chartDataDay),
-    fiveDayData: selectChartDataFiveDay(quotes.chartData.fiveDay)
+    fiveDayData: selectChartDataFiveDay(quotes.chartData.fiveDay),
+    oneMonthData: selectChartDataOneMonth(quotes.chartData.oneMonth),
+    oneYearData: selectChartDataYear(quotes.chartData.oneYear),
+    fiveYearData: selectChartDataYear(quotes.chartData.fiveYear),
+    maxData: selectChartDataYear(quotes.chartData.max)
 });
 
 export default connect(mapStateToProps, null)(Chart);
