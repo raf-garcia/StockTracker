e8a048816cb99c0af0045b877d1743f4fc62430d
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-06-26 15:57:23 -0400

    fix search  on searchList click bug

--- src/components/search/searchInput.jsx
+++ src/components/search/searchInput.jsx
@@ -1,22 +1,25 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import SearchListContainer from '../searchList/searchListContainer';
 
 const SearchInput = ({ searchAction }) => {
   const [searchText, setSearchText] = useState('');
 
-  const handleSubmit = useCallback(event => {
-    event.preventDefault();
-    searchAction(searchText);
-  },
-    [searchAction, searchText]
-  );
+  const handleSubmit = () => {
+
+    //reset the searchText on the input field
+    let search = searchText;
+    setSearchText('');
+
+    searchAction(search);
+    
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input type="text" value={searchText} onChange={input => setSearchText(input.target.value)} size="100%" />
       </form>
-      <SearchListContainer searchText={searchText} searchAction={searchAction}/>
+      <SearchListContainer searchText={searchText} searchAction={handleSubmit}/>
     </>
   );
 }
